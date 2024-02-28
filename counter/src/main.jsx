1d4d8543f75ee8dd29969816b9178a4f35f63294
Sameer0322 <116619265+Sameer0322@users.noreply.github.com>
2024-02-28 19:21:52 +0530

    5th video commit

--- counter/src/main.jsx
+++ counter/src/main.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
- 
-function MyApp(){
-  return (
-    <div>
-      <h1>Custom App !</h1>
-    </div>
-  )
-}
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //
   <React.StrictMode>
-    <App />  {/* this syntax comes from React bundler */}
-    <MyApp/>  
+    <App />
   </React.StrictMode>,
 )
