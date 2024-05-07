0d5cf844760b4a62852abc4c999d591d7db30279
Sameer0322 <116619265+Sameer0322@users.noreply.github.com>
2024-05-07 14:12:29 +0530

    update

---
+++ reduxtoolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,26 @@
+import {cerateSlice, nanoid} from '@reduxjs/toolkit';
+
+const initialState = {
+    todos: [{id:1, text:"Hello World"}]
+}
+
+export const todoSlice = cerateSlice({
+    name: 'todo',
+    initialState,
+    reducers:{
+        addTodo:(state,action) =>{
+            const todo ={
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state,action) =>{
+            state.todos = state.todos.filter((todo)=>todo.id !== action.payload)
+        },
+    } 
+})
+
+export const {addTodo, removeTodo } = todoSlice.actions
+
+export default todoSlice.reducer
